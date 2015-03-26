455a0234951e64842c6b37ff4e8cfd473882aac3
Vaughn <vaughnjack@c02hq2agdjwt.rcs.local>
2015-03-26 15:02:40 -0400

    Initial Commit

---
+++ Stopwatch Xib/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Stopwatch Xib
+//
+//  Created by Vaughn, Jack on 3/26/15.
+//  Copyright (c) 2015 ERHS. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
